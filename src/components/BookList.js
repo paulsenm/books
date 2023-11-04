6e83ccaff29354189b506a5467101e0d5d14e77c
Michael <paulsenm@my.lanecc.edu>
2023-11-04 12:27:08 -0700

    Context is working with counter

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,11 +1,20 @@
+import { useContext } from "react";
+
 import BookShow from "./BookShow";
+import BooksContext from "../context/books";
 
 function BookList({ books, onDelete, onEdit }){
+    const { count, incrementCount } = useContext(BooksContext);
+    //const val = useContext(BooksContext);
     const renderedBooks = books.map((book) => {
        return  <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit} /> ;
     });
     return(
         <div className="book-list">
+            <div>
+                <h3>Count is: {count}</h3>
+                <button onClick={incrementCount}>Increment</button>
+            </div>
             {renderedBooks}
         </div>
     )
